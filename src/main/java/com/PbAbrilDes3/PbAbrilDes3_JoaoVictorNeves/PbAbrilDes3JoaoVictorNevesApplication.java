a1e540c6e027a9adec4a2abc8a09d33d3d2e0422
Joao Neves <joaovictorneves28@hotmail.com.br>
2024-06-25 16:25:07 -0300

    Starting the project

---
+++ src/main/java/com/PbAbrilDes3/PbAbrilDes3_JoaoVictorNeves/PbAbrilDes3JoaoVictorNevesApplication.java
@@ -0,0 +1,13 @@
+package com.PbAbrilDes3.PbAbrilDes3_JoaoVictorNeves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbAbrilDes3JoaoVictorNevesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbAbrilDes3JoaoVictorNevesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PbAbrilDes3/PbAbrilDes3_JoaoVictorNeves/PbAbrilDes3JoaoVictorNevesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PbAbrilDes3.PbAbrilDes3_JoaoVictorNeves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbAbrilDes3JoaoVictorNevesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

3747eb7eff88f6ea17c8075bbc528b15870cd992
Joao Neves <joaovictorneves28@hotmail.com.br>
2024-07-07 22:33:07 -0300

    MicroService calculate completed

---
+++ src/main/java/com/PbAbrilDes3/mscalculate/entity/Calculate.java
@@ -0,0 +1,22 @@
+package com.PbAbrilDes3.mscalculate.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
+@Table(name = "rules")
+public class Calculate implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "category", nullable = false)
+    private String category;
+    @Column(name = "parity", nullable = false)
+    private int parity;
+}
---
+++ src/test/java/com/PbAbrilDes3/mscalculate/MscalculateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PbAbrilDes3.mscalculate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscalculateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

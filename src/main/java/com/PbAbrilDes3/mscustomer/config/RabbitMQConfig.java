5e84c0b22e9b9febf3857806d8613146fa19319b
Joao Neves <joaovictorneves28@hotmail.com.br>
2024-07-09 00:25:33 -0300

    Updating classes

---
+++ src/main/java/com/PbAbrilDes3/mscustomer/config/RabbitMQConfig.java
@@ -0,0 +1,16 @@
+package com.PbAbrilDes3.mscustomer.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_NAME = "paymentQueue";
+
+    @Bean
+    public Queue paymentQueue() {
+        return new Queue(QUEUE_NAME, true);
+    }
+}

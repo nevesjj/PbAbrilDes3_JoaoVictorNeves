c6e800e5fecc10ffca4d102adac2f2f4df5481f4
João Victor Neves <143196007+nevesjj@users.noreply.github.com>
2024-07-09 16:09:23 -0300

    Delete src directory

--- src/main/java/com/PbAbrilDes3/mscalculate/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.PbAbrilDes3.mscalculate.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/PbAbrilDes3/mscalculate/repository/CalculateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.PbAbrilDes3.mscalculate.repository;
-
-import com.PbAbrilDes3.mscalculate.entity.Calculate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CalculateRepository extends JpaRepository<Calculate, Long> {
-}
